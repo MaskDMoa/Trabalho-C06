10321b1aabab418b0fd1535d8dd1fcd20210c463
Hiago <127573527+MaskDMoa@users.noreply.github.com>
2025-04-05 21:54:12 -0300

    Add files via upload

---
+++ Trabalho-C06/src/Memoria.java
@@ -0,0 +1,5 @@
+public class Memoria {
+
+    String Nome;
+    int Capacidade;
+}
---
+++ Trabalho-C06/src/SistemaOp.java
@@ -0,0 +1,4 @@
+public class SistemaOp {
+    String[] Nome = { "Linux Ubuntu", "Windows 8", "Windows 10"};
+    int[] Tipo = {32, 64, 64};
+}
